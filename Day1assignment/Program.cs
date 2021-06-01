5a8a488b2dfefc3c8e3c8a460b1ef09272d6cfb0
Qais Qaisi <Qais.Qaisi@revature.net>
2021-06-01 17:48:49 -0500

    This is the validated integer input for the age

---
+++ Day1assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Day1assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter Your Age :");
+            var ageString = Console.ReadLine();
+            int MyAge ;
+
+            bool parseSuccess = int.TryParse(ageString, out MyAge);
+
+            if (parseSuccess)
+                Console.WriteLine($"Your age is: {MyAge}");
+            else
+                Console.WriteLine("This is not a number!");
+            
+            Console.WriteLine("Enter Your Name :");
+            String MyName = Console.ReadLine();
+            Console.WriteLine($"Your  name is {MyName} and your age is {MyAge}");
+        }
+    }
+}
